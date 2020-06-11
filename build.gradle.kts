9443cc9bb04acb5e1039081c99cd95f23a2ae2fa
Markus Ressel <mail@markusressel.de>
2020-06-11 12:12:28 +0200

    bump gradle build tools version to 4.0.0 bump gradle version to 6.1.1 bump kotlin version to 1.3.72

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ buildscript {
     extra["minSdkVersion"] = 16
     extra["compileSdkVersion"] = 29
     extra["targetSdkVersion"] = 29
-    extra["kotlinVersion"] = "1.3.61"
+    extra["kotlinVersion"] = "1.3.72"
 
     repositories {
         mavenCentral()
@@ -13,7 +13,7 @@ buildscript {
 
     dependencies {
         val kotlinVersion = property("kotlinVersion") as String
-        classpath("com.android.tools.build:gradle:3.6.1")
+        classpath("com.android.tools.build:gradle:4.0.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath("com.otaliastudios.tools:publisher:0.1.5")
     }
