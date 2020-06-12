7edbb36b30905fdec462d613b380f51a4fd2f6d4
Markus Ressel <mail@markusressel.de>
2020-06-12 22:55:57 +0200

    explicitly cancel fling when cancelling animations

--- library/src/main/java/com/otaliastudios/zoom/ZoomEngine.kt
+++ library/src/main/java/com/otaliastudios/zoom/ZoomEngine.kt
@@ -159,6 +159,7 @@ internal constructor(context: Context) : ZoomApi {
     // Internal
     private lateinit var container: View
     private val callbacks = Callbacks()
+
     @Suppress("LeakingThis")
     private val dispatcher = UpdatesDispatcher(this)
     private val stateController = StateController(callbacks)
@@ -516,7 +517,7 @@ internal constructor(context: Context) : ZoomApi {
      */
     internal fun setContainer(container: View) {
         this.container = container
-        this.container.addOnAttachStateChangeListener(object: View.OnAttachStateChangeListener {
+        this.container.addOnAttachStateChangeListener(object : View.OnAttachStateChangeListener {
             override fun onViewAttachedToWindow(view: View) {
                 view.viewTreeObserver.addOnGlobalLayoutListener(callbacks)
             }
@@ -833,7 +834,10 @@ internal constructor(context: Context) : ZoomApi {
      * @return true if anything was cancelled, false otherwise
      */
     override fun cancelAnimations(): Boolean {
-        if (stateController.isFlinging() || stateController.isAnimating()) {
+        if (stateController.isFlinging()) {
+            scrollFlingDetector.cancelFling()
+            return true
+        } else if (stateController.isAnimating()) {
             stateController.makeIdle()
             return true
         }
